562c796e0ea785bd36a179f0432d80c9b76ecc2c
usmanshery <usman.shery@gmail.com>
2020-07-02 00:39:22 +0500

    All dialogues done, re-recording remains big issue

--- src/Thankyou.svelte
+++ src/Thankyou.svelte
@@ -3,9 +3,32 @@
 
   export let location;
 
+
 </script>
 
 <style>
+  .dialogue{
+    width: 100%;
+    height: auto;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);  
+  }
+
+  .dialogue_text_box{
+    background: rgb(95, 95, 95);
+    padding: 30px;
+    margin-bottom: 10px;
+  }
+
+  .dialogue_text_box>p{
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 100;
+    font-size: 25px;
+    
+  }
+
   main {
     text-align: center;
     padding: 1em;
@@ -13,11 +36,26 @@
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
+  .button {
+    color: #fff !important;
     text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+    text-decoration: none;
+    font-size: 20px;
+    background: #434343;
+    padding: 10px;
+	  width: 100px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
+    transition: all 0.4s ease 0s;
+  }
+  .button:hover {
+    background: #434343;
+    letter-spacing: 1px;
+    -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+    -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+    box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+    transition: all 0.4s ease 0s;
   }
   @media (min-width: 640px) {
     main {
@@ -27,10 +65,12 @@
 </style>
 
 <main>
-  <section id="Banner" class="content-section">
-    <div class="container content-wrap text-center">
-      <h1>Thank You!</h1>
+    <div class="dialogue">
+
+      <div class="dialogue_text_box">
+        <p>
+          All done! Thankyou for your response.
+        </p>
+      </div>
     </div>
-  </section>
-  <!-- Header Ends -->
 </main>
--- src/Video.svelte
+++ src/Video.svelte
@@ -5,16 +5,28 @@
 
   import { onDestroy } from "svelte";
 
-  var uploadVideo = false;
+  var videoRecorder = null;
+
+  var checkPhase = true;
+  var recordingPhase = false;
+  var uploadPhase = false;
+
   var isRecording = false;
+  var isPlayingback = false;
   var uploading = false;
-  var player = null;
+  var hasUploaded = false;
+
+  var lastRecording = null;
 
   onMount(function() {
     onDestroy(() => console.log("onDestroy"));
 
-    var options = {
-      controls: true,
+    // apply some workarounds for opera browser
+    applyVideoWorkaround();
+
+    // recording
+    var videoRecorderOptions = {
+      controls: false,
       fill: true,
       responsive: true,
       fluid: false,
@@ -28,51 +40,109 @@
       }
     };
 
-    // apply some workarounds for opera browser
-    applyVideoWorkaround();
-
-    player = videojs("myVideo", options, function() {
+    videoRecorder = videojs("videoRecorder", videoRecorderOptions, function() {
       // print version information at startup
       var msg =
-        "Using video.js " +
+        "2. Using video.js " +
         videojs.VERSION +
         " with videojs-record " +
         videojs.getPluginVersion("record") +
         " and recordrtc " +
         RecordRTC.version;
       videojs.log(msg);
-      player.record().getDevice();
+      videoRecorder.record().getDevice();
     });
+
+    videoRecorder.controlBar.progressControl.disable();
 
     // error handling
-    player.on("deviceError", function() {
-      console.log("device error:", player.deviceErrorCode);
+    videoRecorder.on("deviceError", function() {
+      console.log("device error:", videoRecorder.deviceErrorCode);
     });
 
-    player.on("error", function(element, error) {
+    videoRecorder.on("error", function(element, error) {
       console.error(error);
     });
 
     // user clicked the record button and started recording
-    player.on("startRecord", function() {
+    videoRecorder.on("startRecord", function() {
       console.log("started recording!");
-      isRecording = true;
     });
 
     // user completed recording and stream is available
-    player.on("finishRecord", function() {
-      isRecording = false;
-      uploadVideo = true;
-      console.log("finished recording: ", player.recordedData);
+    videoRecorder.on("finishRecord", function() {
+      console.log("finished recording: ", videoRecorder.recordedData);
+      lastRecording = videoRecorder.recordedData;
+
+      var recorder = document.getElementById('videoRecorder');
+      var player = document.getElementById('videoPlayer');
+
+      recorder.style.display = "none";
+      player.style.display = "block";
+
+      player.src = window.URL.createObjectURL(lastRecording);
     });
   });
 
   function startRecording() {
-    if (player.record().isRecording()) {
-      player.record().stop();
-    } else {
-      player.record().start();
+    if (!videoRecorder.record().isRecording()) {
+      videoRecorder.record().start();
+      isRecording = true;
     }
+  }
+
+  function stopRecording() {
+    if (videoRecorder.record().isRecording()) {
+      videoRecorder.record().stop();
+      isRecording = false;
+      recordingPhase = false;
+      uploadPhase = true;
+    }
+  }
+
+  function playbackRecording(){
+    var player = videojs('videoPlayer');
+    player.play();
+    player.on('ended', function(){
+      isPlayingback = false;
+      // player.dispose();
+      // // mainDiv
+
+      // let reMake = document.createElement('video');
+      // reMake.id = 'videoPlayer';
+      // reMake.class = 'video-js';
+      // reMake.style = 'display: none;';
+      // document.getElementById('mainDiv').appendChild(reMake);
+    });
+    isPlayingback = true;
+  }
+
+  function resetRecording(){
+    recordingPhase = true;
+
+    uploadPhase = false;
+    isRecording = false;
+    isPlayingback = false;
+    uploading = false;
+    hasUploaded = false;
+
+    lastRecording = null;
+
+    var recorder = document.getElementById('videoRecorder');
+    var player = document.getElementById('videoPlayer');
+
+    recorder.style.display = "block";
+    player.style.display = "none";
+
+  }
+
+  function uploadVideo(){
+
+  }
+
+  function readyForRecording(){
+    checkPhase = false;
+    recordingPhase = true;
   }
 
   function handleClick() {
@@ -95,7 +165,7 @@
       params: { Bucket: albumBucketName }
     });
 
-    var uploadFile = player.recordedData;
+    var uploadFile = videoRecorder.recordedData;
     var timestamp = new Date().getTime().toString();
     var uploadFile = new File([uploadFile], timestamp);
     var albumPhotosKey = "videostuff" + "/";
@@ -115,21 +185,24 @@
       }).promise();
     promise.then(
       function(data) {
-        player.dispose();
+        videoRecorder.dispose();
         navigate("/thankyou", { replace: true });
       },
       function(err) {
         return alert("There was an error uploading your photo: ", err.message);
       }
     );
+
+    hasUploaded = true;
   }
 </script>
 
 <style>
+
   /* change player background color */
   .content .video-js {
-    height: 95%;
-    width: 95%;
+    height: 100%;
+    width: 100%;
     position: relative;
     top: 0%;
     left: 0%;
@@ -137,28 +210,87 @@
 
   .videooverlay {
     position: absolute;
-    /* top:0;
-        left:0;
-        right:0;
-        bottom:0; */
-    /* width: 100%; */
-    width: inherit;
-    height: 100px;
-    background-color: rgba(255, 255, 255, 0.49);
+    width: 100%;
+    height: auto;
+    background-color: rgba(70, 70, 70, 0.8);
     text-align: center;
     vertical-align: middle;
-    line-height: 90px;
+  }
+
+  .videooverlay>p{
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 100;
+    font-size: 25px;
+  }
+
+  .buttonNext{
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 20px;
+    background: #434343;
+    /* width: 120px; */
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+  }
+
+  .buttonRecord{
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 20px;
+    background: #fa1a70;
+    /* width: 130px; */
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+  }
+
+  .buttonRetry{
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 20px;
+    background: #434343;
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+    margin-left: -75px;
+  }
+
+  .buttonSubmit{
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 20px;
+    background: #434343;
+    border-radius: 5px;
+    transition: all 0.4s ease 0s;
+    margin-left: 75px;
+  }
+
+  .buttonPlay{
+    position: absolute;
+    background: url("/play.png");
+    background-size: cover;
+    display: inline-block;
+    border: none;
+    width: 50px;
+    height: 50px;
+  }
+
+  .buttonMidBottom{
+    position: absolute;
+    bottom: 10%;
+    padding: 10px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
+    width: auto;
   }
 
   .content {
-    height: 95vh;
-    width: 95vw;
+    height: 100vh;
+    width: 100%;
     padding: 0;
-    /* margin: 0;
-    display: -webkit-box;
-    display: -moz-box;
-    display: -ms-flexbox;
-    display: -webkit-flex; */
     display: flex;
     align-items: center;
     justify-content: center;
@@ -171,6 +303,7 @@
     width: 60%;
     margin: 0 auto;  /* (height of inner div) / 2 + padding */
     padding: 3px;
+    position: absolute;
   }
 
   #progressbar>div {
@@ -182,34 +315,61 @@
   }
 </style>
 
-<div class="content">
-  <video
-    id="myVideo"
-    playsinline
-    class="video-js vjs-theme-defualt vjs-big-play-centered" />
-  <div class="videooverlay">
-    {#if uploading}
-      <p>uploading your video</p>
-    {:else}
-      <p>Check yourself and start recording when you are ready</p>
-    {/if}
+<main>
+    <div id="mainDiv" class="content">
+      
+      <video-js id="videoRecorder" playsinline class="video-js vjs-theme-defualt vjs-big-play-centered" />
 
-    {#if uploading}
-      <div id="progressbar">
-        <div />
-      </div>
-    {:else if uploadVideo}
-      {#if isRecording}
-        <button on:click|once={startRecording}>Done Recording</button>
-      {:else}
-        <button on:click|once={startRecording}>Restart Recording</button>
+      <video id="videoPlayer" class="video-js" style="display:none;">
+        
+      </video>
+
+      {#if uploading}
+          <div id="progressbar"><div></div></div>
       {/if}
-      <button on:click|once={handleClick}>Upload</button>
-    {:else if isRecording}
-      <button on:click|once={startRecording}>Done Recording</button>
-    {:else}
-      <button on:click|once={startRecording}>Begin Recording</button>
-    {/if}
 
-  </div>
-</div>
+      <div class="videooverlay">
+
+        {#if checkPhase}
+          <p>Check yourself and start recording when you are ready</p>
+        {/if}
+
+        {#if recordingPhase}
+          {#if !isRecording}
+            <p>Record a short video telling us what you like about your new Casper mattress. 🙌🏻 Hit record when ready 👇🏻</p>
+          {/if}
+        {/if}
+
+        
+
+      </div> <!-- overlay div -->
+
+      {#if checkPhase}
+        <button class="buttonMidBottom buttonNext" on:click|once={readyForRecording}>&#10132;&#8287;&#8287;Next</button>
+      {/if}
+
+      {#if recordingPhase}
+        {#if !isRecording}
+          <button class="buttonMidBottom buttonRecord" on:click|once={startRecording}>&#9898;&#8287;&#8287;RECORD</button>
+        {/if}
+
+        {#if isRecording}
+          <button class="buttonMidBottom buttonRecord" on:click|once={stopRecording}>&#11036;&#8287;&#8287;STOP</button>
+        {/if}
+      {/if}
+
+      {#if uploadPhase}
+        {#if !uploading}
+          {#if !isPlayingback}
+            <button class="buttonPlay" on:click|once={playbackRecording}></button>
+          {/if}
+          <button class="buttonMidBottom buttonRetry" on:click|once={resetRecording}>&#8635;&#8287;&#8287;RETRY</button>
+          <button class="buttonMidBottom buttonSubmit" on:click|once={handleClick}>&#xf0ee;&#8287;&#8287;SUBMIT</button>
+        {/if}
+
+        
+
+      {/if}
+
+    </div> <!-- content div -->
+</main>
